141b4ecc24a628772ebef46d9378db548d0b6db7
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-09-30 13:30:43 +0530

    Implement layout and header components, update dependencies
    - Added layout and header components for the application structure.
    - Created navigation items and user dropdown components for enhanced user interaction.
    - Introduced search command component for stock search functionality.
    - Updated package.json and package-lock.json to include new Radix UI dependencies.
    - Removed the old page component to streamline the application structure.
    - Added constants for navigation items and various configurations for widgets.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex min-h-screen home-wrapper'>
+      <div className='home-section'>
+        <div className='home-section-left'>
+          <h1 className='text-4xl font-bold'>Welcome to Signalist</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ components/SearchCommand.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchCommand = ({ renderAs = 'button', label = 'Add stock', initialStocks }: SearchCommandProps) => {
+  return (
+    <div>SearchCommand</div>
+  )
+}
+
+export default SearchCommand
